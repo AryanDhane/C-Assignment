7a31f094e5ad0d4b3ae560f1b18ea3d48c77c47c
AryanDhane <dhanearyan150074@gmail.com>
2026-07-27 13:57:34 +0530

    ..

---
+++ Q5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float real1, imag1, real2, imag2, realSum, imagSum;
+
+    cout << "Enter real part of first complex number: ";
+    cin >> real1;
+    cout << "Enter imaginary part of first complex number: ";
+    cin >> imag1;
+
+    cout << "Enter real part of second complex number: ";
+    cin >> real2;
+    cout << "Enter imaginary part of second complex number: ";
+    cin >> imag2;
+
+    realSum = real1 + real2;
+    imagSum = imag1 + imag2;
+
+    cout << "Sum = " << realSum << " + " << imagSum << "i" << endl;
+    return 0;
+}
+
+
+
+// output
+
+//
